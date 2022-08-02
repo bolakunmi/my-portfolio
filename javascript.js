62b6eb72868fb3e13388197b0165560a03156a5d
Bolakunmi Oluwayemi <52560352+bolakunmi@users.noreply.github.com>
2022-08-02 07:42:21 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,27 +1,29 @@
-const toggler = document.querySelector(".light_dark");
+const light_dark = document.querySelector(".light_dark");
 const light = document.querySelector(".light");
 const body = document.querySelector("body");
 const small_button = document.querySelector(".small_button");
 const header = document.querySelector("header");
-const nav_toggler = document.querySelector(".nav_toggler");
+const nav_toggler_img = document.querySelector(".nav_toggler img");
 const nav_list = document.querySelector(".hidden ul");
 const selected_item = document.querySelectorAll(".hidden a");
 const move_up = document.querySelector(".move_up");
+const more = document.querySelector(".more");
+const cancel = document.querySelector(".close");
+const min_list = document.querySelector(".list");
 
 body.style.backgroundColor = "black";
-toggler.addEventListener("click", function () {
+light_dark.addEventListener("click", function () {
   if (body.style.backgroundColor == "white") {
     // change the image src
     body.style.backgroundColor = "black";
     body.style.color = "white";
     move_up.src = "./icons8-up-48 (1).png";
-    light.src = "./lightbulb-off-fill.svg";
+    light.src = "./moon.png";
   } else if ((body.style.backgroundColor = "black")) {
-    light.src = "./lightbulb.svg";
+    light.src = "./sun.png";
     body.style.backgroundColor = "white";
     body.style.color = "black";
     move_up.src = "./icons8-up-48.png";
-    light.src = "./lightbulb.svg";
   }
 });
 
@@ -45,6 +47,28 @@ move_up.addEventListener("click", function () {
 //   }
 // });
 
+// nav_toggler_img.addEventListener("click", function () {
+//   if (nav_toggler_img.src = "./more.png") {
+//     nav_toggler_img.src = "./close.png"
+//     console.log('close')
+
+//   } else if (nav_toggler_img.src = "./close.png") {
+//     nav_toggler_img.src = "./more.png"
+//     console.log('display')
+//   }
+// })
+
+/* nav_toggler.addEventListener("click", function () {
+if (nav_toggler.backgroundColor = url("./more.png")) {
+  nav_toggler.backgroundColor = url("./close.png")
+  console.log('close')
+
+} else if (nav_toggler.backgroundColor = url("./more.png")) {
+  nav_toggler.backgroundColor = url("./more.png")
+  console.log('display more')
+}
+})*/
+
 // to jump around selected items
 for (let i = 0; i < 3; i++) {
   nav_list.children[i].addEventListener("click", function () {
@@ -55,8 +79,29 @@ for (let i = 0; i < 3; i++) {
   });
 }
 
+for (let i = 0; i < 3; i++) {
+  min_list.children[i].addEventListener("click", function () {
+    for (let j = 0; j < 3; j++) {
+      min_list.children[j].classList.remove("selected");
+    }
+    min_list.children[i].classList.add("selected");
+  });
+}
 
+// let options = "more"
+// nav_toggler_img.addEventListener("click", function () {
+//   if (options == "more") {
+//     more.style.src = "./more.png"
+//     console.log(options)
+//     options = "close";
+//   } else if (options == "close") {
+//     more.style.src = "./close.png"
 
+//     console.log("it shoudl go back")
+//     options = "more"
+//   }
+
+// })
 
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
